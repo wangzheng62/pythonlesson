68708bfd429988ecd9aa88c154e0fbc9bfe5cda8
wangzheng62 <363579018@qq.com>
2016-09-27 17:32:00 +0800

    0927

---
+++ gui.py
@@ -0,0 +1,15 @@
+#hello
+print('Hello,welcome to pythonlesson3.5 gui')
+#import
+import sys
+import os
+#initialization
+sys.path.append('D:\\workspace\\python\\pythonlesson')
+#create folder
+#create file
+filename='D:\\test'
+mode='w+'
+f=open(filename,mode)
+f.close()
+#exit()
+exit()
---
+++ python开发技术详解/_section_5.py
@@ -0,0 +1,6 @@
+#__name__,__doc__
+#buildin
+#apply(func[,args[,kwargs]])
+#filter(func or None,sequence)->list tuple string
+#reduce(func,sequence[,initial])->value
+#map(func,sequence[,sequence])->list
